0cf37bac7ca231151994d73e454f56cd0288de49
m-peeler <mdavispeeler@gmail.com>
2024-06-10 22:34:46 -0400

    Test

--- api/app.ts
+++ api/app.ts
@@ -1,5 +1,3 @@
 import app from '.';
 
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log('Server is listening on PORT:', port));
+app.listen('/api', () => console.log('Server is alive.'));
